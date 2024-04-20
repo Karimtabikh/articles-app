17038873614426a34cc73f976ea48b9400a4af97
Karimtabikh <karim.tabikh@outlook.com>
2024-04-20 16:23:48 +0300

    Lint and Formatting Fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 import { Config } from "tailwindcss/types";
 
 const config: Config = {
-	content: ["./index.html", "./src/**/*.tsx"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
 
 export default config;
