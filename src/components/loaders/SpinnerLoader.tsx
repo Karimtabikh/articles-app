bbe1dfeb19425a167bc55b88ebb19a23f98216b8
Karimtabikh <karim.tabikh@outlook.com>
2024-04-24 19:57:23 +0300

    Resolved converstations

--- src/components/loaders/SpinnerLoader.tsx
+++ src/components/loaders/SpinnerLoader.tsx
@@ -1,10 +1,7 @@
 const SpinnerLoader = () => (
-  <>
     <div className="absolute inset-x-0 top-0 flex justify-center py-10 rounded-b-full bg-slate-800/75">
       <div className="rounded-full w-14	h-14 animate-spin border-8 border-white border-t-8 border-t-blue-200"></div>
     </div>
-    
-  </>
 );
 
 export default SpinnerLoader;
