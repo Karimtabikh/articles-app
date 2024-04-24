94aa3581e16dc8f34b403cf853b31565df037840
Karimtabikh <karim.tabikh@outlook.com>
2024-04-24 20:05:07 +0300

    Resolve convservations

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
+// biome-ignore lint/style/noNonNullAssertion: the root will always be defined since we set it in the html document
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
