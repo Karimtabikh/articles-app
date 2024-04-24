44062bf51210a681ab05407423f333120647a135
Karimtabikh <karim.tabikh@outlook.com>
2024-04-24 18:13:35 +0300

    Shad Cn Added

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button"
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -8,6 +9,8 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Form</h3>
+      <Button>Click me</Button>
+
     </div>
   )
 }
